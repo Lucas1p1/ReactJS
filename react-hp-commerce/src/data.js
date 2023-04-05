d070dd8b8b647ce7deaf8b72c5a9b524d046346d
Osunde Adekoya Adedotun <adekoyadedeji16@gmail.com>
2023-04-05 04:00:52 +0100

    An e-commerce website built with react

---
+++ react-hp-commerce/src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Spectre x360 Laptop",
+    img: "img/product-1.png",
+    price: 700000,
+    company: "HP",
+    info:
+      "The HP Spectre x360 Laptop is powered by an Intel® Processor[2] that adapts to you for superior multi-tasking. With a taller display, you can view more content with less scrolling. Look your best thanks to an intelligent 5MP camera[3] that keeps you in-frame and well-lit. Plus, protect your well-being with screen time and distance reminders.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Omen",
+    img: "img/product-2.png",
+    price: 720000,
+    company: "HP",
+    info:
+      " The OMEN Laptop is ready to go wherever you are. Enjoy desktop-level power with an AMD processor[1] and a modern graphics card, both regulated by OMEN Tempest Cooling. The screen is equally impressive with high resolution and TUV Eyesafe Low Blue Light[2]. This gaming laptop even lets you expand with plenty of USB ports[3,4].",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Envy x360 13",
+    img: "img/product-3.png",
+    price: 8,
+    company: "hP",
+    info:
+      "Seamlessly create with the compact HP Envy x360 13. With the color-calibrated display, you get the ultimate color accuracy. Stay productive with a long battery life and the Intel® Processor[1] and graphics. Combine the power of devices with HP Palette. Look your best on the 5MP[2] GlamCam with Appearance Filter, Backlight Adjustment & Auto Frame.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Spectre x360",
+    img: "img/product-4.png",
+    price: 180000000,
+    company: "HP",
+    info:
+      "The HP Spectre x360 Laptop is powered by an Intel® Processor[2] that adapts to you for superior multi-tasking. With a taller display, you can view more content with less scrolling. Look your best thanks to an intelligent 5MP camera[3] that keeps you in-frame and well-lit. Plus, protect your well-being with screen time and distance reminders.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Dragonfly Pro",
+    img: "img/product-5.png",
+    price: 555000,
+    company: "HP",
+    info:
+      "The adaptable HP Dragonfly Pro delivers a whole new level of speed, power, and performance when you need it, with a vivid touchscreen display, ample storage, and collaborative audio-visual tools—all backed by a sustainable design, superior battery life, and one-touch access to 24/7 live support[2].",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "ZBook Studio G9 Mobile Workstation",
+    img: "img/product-7.png",
+    price: 1700000000,
+    company: "HP",
+    info:
+      "Be ready when inspiration strikes. Packed with professional performance in an incredibly sleek PC that comfortably fits in your bag, the ZBook Studio is engineered to get you in the zone and help you stay focused. Now you can build complex assemblies, render in real time or visualize data—from anywhere.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Victus Laptop",
+    img: "img/product-6.png",
+    price: 30000000,
+    company: "HP",
+    info:
+      "The  HP VictusLaptop is made for peak PC gaming. This sleek machine touts a 12th Gen Intel® Core™ processor[1] and a modern graphics card. This laptop’s design is just as impressive as its hardware with plenty of color options[2], updated thermals, a full all-in-one gaming keyboard, and an HD camera with Temporal Noise Reduction.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "HP Elite Dragonfly G3 - Wolf Pro Security Edition",
+    img: "img/product-8.png",
+    price: 2,
+    company: "HP",
+    info:
+      "Change the dynamics of the room with the meticulously crafted, ultra-light HP Elite Dragonfly featuring HP Wolf Security for Business.[5] This beautifully designed, laptop provides a stunning collaboration experience powered by HP Presence[4] with AI-driven crystal-clear audio and an HD display. Drive productivity with a powerful Intel® Core™ processor[2], a Wi-Fi 6E[9] connection, and contextual intelligence.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Spectre x360 Laptop",
+    img: "img/product-1.png",
+    price: 700000,
+    company: "HP",
+    info:
+      "The HP Spectre x360 Laptop is powered by an Intel® Processor[2] that adapts to you for superior multi-tasking. With a taller display, you can view more content with less scrolling. Look your best thanks to an intelligent 5MP camera[3] that keeps you in-frame and well-lit. Plus, protect your well-being with screen time and distance reminders.",
+    inCart: false,
+    count: 0,
+    total: 0
+};
